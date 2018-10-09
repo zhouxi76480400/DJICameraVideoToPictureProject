3e69acec8711c330d7cae28caaf95226d4108664
zhouxi <zhouxi121@live.com>
2018-10-09 15:32:09 +0900

    modify

--- video_to_picture/video_to_picture.cpp
+++ video_to_picture/video_to_picture.cpp
@@ -27,44 +27,67 @@ int convert_nv12_to_bmp(int width_pixel, int height_pixel, string nv12_path, str
             in_stream.read(buffer,file_size);
             in_stream.close();
             cout << "input file size: " << strlen(buffer) << endl;
-            char * yv12buffer = (char *) malloc(file_size +1);
-            memcpy(yv12buffer,buffer,file_size);
-            // convert NV12 to YV12
-            convertNV12ToYV12((uint8_t *)buffer,(uint8_t *)yv12buffer,width_pixel,height_pixel);
-            delete [] buffer;
-            buffer = NULL;
-            // convert I420 to BGR
-            int bgr_size = width_pixel * height_pixel * 3;
-            char * bgrbuffer = (char *) malloc(bgr_size + 1);
-            bool isSuccessToConvertBGR = convertYV12ToBGR24_Table((uint8_t *)yv12buffer,(uint8_t *)bgrbuffer,width_pixel,height_pixel);
-            if(isSuccessToConvertBGR) {
-                delete [] yv12buffer;
-                yv12buffer = NULL;
-                char * bmp_header_buffer = (char *)malloc(100);// fix a problem with linux buffer overflow
-                createBMPHeader((uint8_t *)bmp_header_buffer,width_pixel,height_pixel,bgr_size);
+            // define a out buffer
+            int out_buffer_size = width_pixel * height_pixel * 3 + Bmp_FileHeader_Size + Bmp_Info_Size;
+            char * out_buffer = new char[out_buffer_size];
+            status = convert_nv12_to_bmp_buffer(width_pixel,height_pixel,buffer,out_buffer);
+            if(status == convert_nv_12_to_bmp_status_success){
                 ofstream out_stream;
                 out_stream.open(save_path, std::ofstream::binary);
-                out_stream.write(bmp_header_buffer,Bmp_Info_Size + Bmp_FileHeader_Size);
-                out_stream.write(bgrbuffer,bgr_size);
+                out_stream.write(out_buffer,out_buffer_size);
                 out_stream.close();
-                delete [] bgrbuffer;
-                bgrbuffer = NULL;
-                delete [] bmp_header_buffer;
-                bmp_header_buffer = NULL;
-                cout << "output file size: " << Bmp_Info_Size + Bmp_FileHeader_Size + bgr_size << endl;
-                status = convert_nv_12_to_bmp_status_success;
-            }else{
-                status = convert_nv_12_to_bmp_status_failed;
+                delete [] out_buffer;
+                out_buffer = NULL;
             }
+            delete [] buffer;
+            buffer = NULL;
         }
-    }else{
+    } else {
         status = convert_nv_12_to_bmp_status_file_not_exist_or_not_permission;
     }
     return status;
 }
 
+char * memcat(char *dest, size_t dest_len, const char *src, size_t src_len) {
+    memcpy(dest+dest_len, src, src_len);
+    return dest;
+}
 
-int convert_dji_camera_nv12_to_bmp(string nv12_path, string save_path){
+int convert_nv12_to_bmp_buffer(int width_pixel, int height_pixel, char * in_buffer, char * out_buffer){
+    int status = convert_nv_12_to_bmp_status_failed;
+    if(in_buffer != NULL && out_buffer != NULL) {
+        int file_size = width_pixel * height_pixel * 3 / 2;
+        char * yv12buffer = (char *) malloc(file_size +1);
+        memcpy(yv12buffer,in_buffer,file_size);
+        // convert NV12 to YV12
+        convertNV12ToYV12((uint8_t *)in_buffer,(uint8_t *)yv12buffer,width_pixel,height_pixel);
+        // convert YV12 to BGR
+        int bgr_size = width_pixel * height_pixel * 3;
+        char * bgrbuffer = (char *) malloc(bgr_size + 1);
+        bool isSuccessToConvertBGR = convertYV12ToBGR24_Table((uint8_t *)yv12buffer,(uint8_t *)bgrbuffer,width_pixel,height_pixel);
+        if(isSuccessToConvertBGR) {
+            delete [] yv12buffer;
+            yv12buffer = NULL;
+            char * bmp_header_buffer = (char *)malloc(Bmp_FileHeader_Size + Bmp_FileHeader_Size + bgr_size);// fix a problem with linux buffer overflow
+            memcat(bmp_header_buffer,Bmp_FileHeader_Size + Bmp_Info_Size,bgrbuffer,bgr_size);
+            createBMPHeader((uint8_t *)bmp_header_buffer,width_pixel,height_pixel,bgr_size);
+            memcpy(out_buffer,bmp_header_buffer,Bmp_Info_Size+Bmp_FileHeader_Size+bgr_size);
+            cout << "output file size: " << Bmp_Info_Size+Bmp_FileHeader_Size+bgr_size << endl;
+            delete [] bmp_header_buffer;
+            delete [] bgrbuffer;
+            bmp_header_buffer = NULL;
+            bgrbuffer = NULL;
+            status = convert_nv_12_to_bmp_status_success;
+        } else{
+            status = convert_nv_12_to_bmp_status_failed;
+        }
+    }else {
+        status = convert_nv_12_to_bmp_status_file_not_exist_or_not_permission;
+    }
+    return status;
+}
+
+int convert_dji_camera_nv12_to_bmp(string nv12_path, string save_path) {
     int status = convert_nv12_to_bmp(1280,720,nv12_path,save_path);
     return status;
 }
--- video_to_picture/video_to_picture.h
+++ video_to_picture/video_to_picture.h
@@ -24,6 +24,15 @@ using namespace std;
  */
 int convert_nv12_to_bmp(int with_pixel, int height_pixel, string nv12_path, string save_path);
 
+/**
+ * convert method with buffer
+ * @param width_pixel
+ * @param height_pixel
+ * @param in_buffer
+ * @param out_buffer
+ * @return
+ */
+int convert_nv12_to_bmp_buffer(int width_pixel, int height_pixel, char * in_buffer, char * out_buffer);
 
 /**
  * only for dji z3 x3
@@ -32,5 +41,6 @@ int convert_nv12_to_bmp(int with_pixel, int height_pixel, string nv12_path, stri
  * @return
  */
 int convert_dji_camera_nv12_to_bmp(string nv12_path, string save_path);
+
 
 #endif //DJICAMERAVIDEOTOPICTUREPROJECT_VIDEO_TO_PICTURE_H
